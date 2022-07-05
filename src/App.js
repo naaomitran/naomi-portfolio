e101cd53ad76792bbd3ab4ee9ddba164621cdc4c
Naomi Tran <naaomitran@gmail.com>
2022-07-05 17:53:45 -0500

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -6,42 +6,17 @@ import ComingSoon from './ComingSoon';
 
 function App() {
   return (
-    // <HashRouter basename="/subdirectory"> 
-
-    // <Router>
-    //   <Switch>
-    //     <Route {`${process.env.PUBLIC_URL}/about`} component={About} />
-    //     <Route path={`${process.env.PUBLIC_URL}/projects`} component={Project} />
-    //     <Route path={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
-    //     <Route path={`${process.env.PUBLIC_URL}/resume`}>
-    //       <Document file={Resume}>
-    //       <Page />
-    //       </Document>
-    //     </Route>
-    //     <Route exact path={`${process.env.PUBLIC_URL}/`} component={HP} />
-
-    //   </Switch>
-
-    // </Router>
-
-    // </HashRouter>
-
-    // <HashRouter basename="/subdirectory"> 
     <Router>
       <Routes>
-        {/* <Route path={`${process.env.PUBLIC_URL}/snack`} component={Snack} />
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} /> */}
-        <Route path="/snack" element={<Snack />} />
-        <Route path="/barrtech-mechanical" element={<Barr />} />
-        <Route path="/personal-projects" element={<Barr />} />
-        <Route path="/coming-soon" element={<ComingSoon />} />
-
+        <Route path={`${process.env.PUBLIC_URL}/snack`} element={<Snack />} />
+        <Route path={`${process.env.PUBLIC_URL}/barrtech-mechanical`} element={<Barr />} />
+        <Route path={`${process.env.PUBLIC_URL}/coming-soon`} element={<ComingSoon />} />
+        <Route path={`${process.env.PUBLIC_URL}/coming-soon`} element={<ComingSoon />} />
 
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>} exact />
       </Routes>
 
     </Router>
-    // </HashRouter>
   );
 }
 
